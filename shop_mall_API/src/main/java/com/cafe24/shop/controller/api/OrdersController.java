2a2c6532a3a5bf7436d36a96215ab2d4a9b32707
MaximSungmo <ksm5318@naver.com>
2019-08-02 18:11:03 +0900

    ORDER TEST

---
+++ shop_mall_API/src/main/java/com/cafe24/shop/controller/api/OrdersController.java
@@ -0,0 +1,98 @@
+package com.cafe24.shop.controller.api;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cafe24.shop.dto.JSONResult;
+import com.cafe24.shop.service.CustomerService;
+import com.cafe24.shop.service.OrdersService;
+import com.cafe24.shop.vo.OrdersDetailVo;
+import com.cafe24.shop.vo.OrdersVo;
+
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+
+@Controller("ordersAPIController")
+@RequestMapping("/api/orders")
+public class OrdersController {
+
+	@Autowired
+	OrdersService ordersService;
+	@Autowired
+	CustomerService customerService;
+	
+
+	@ApiOperation(value = "주문 생성")
+    @ApiImplicitParams({
+        @ApiImplicitParam(name = "OrdersVo", value = "OrdersVo", dataType = "OrdersVo", paramType = "body"),
+        @ApiImplicitParam(name = "OrdersDetailVo", value = "OrdersDetailVo", dataType = "OrdersDetailVo", paramType = "body"),
+
+    })
+	@ResponseBody
+	@RequestMapping(value = { "" }, method = RequestMethod.PUT)
+	public ResponseEntity<JSONResult> add_cart(
+			@RequestBody OrdersVo ordersvo
+			, @RequestBody List<OrdersDetailVo> orders_detail_list) {
+		
+		Boolean inserted_orders_result=ordersService.add_orders(ordersvo, orders_detail_list);		
+		return ResponseEntity.status(HttpStatus.OK).body(JSONResult.success(inserted_orders_result));
+	}
+	
+	@ApiOperation(value = "주문 조회")
+    @ApiImplicitParams({
+    	 @ApiImplicitParam(name = "customer_no", value = "customer_no", dataType = "long", paramType = "path"),
+    })
+	@ResponseBody
+	@RequestMapping(value = { "/{customer_no}" }, method = RequestMethod.GET)
+	public ResponseEntity<JSONResult> get_orders(
+			@PathVariable(value="customer_no") Long customer_no) {
+		List<OrdersVo> orders_list = ordersService.get_orders(customer_no);
+		return ResponseEntity.status(HttpStatus.OK).body(JSONResult.success(orders_list));
+	}
+	
+	
+	@ApiOperation(value = "관리자 주문 조회")
+    @ApiImplicitParams({
+    	 @ApiImplicitParam(name = "customer_no", value = "customer_no", dataType = "long", paramType = "path"),
+    	 @ApiImplicitParam(name = "product_detail_no", value = "product_detail_no", dataType = "long", paramType = "path"),
+    })
+	@ResponseBody
+	@RequestMapping(value = { "/{customer_no}/{product_detail_no}" }, method = RequestMethod.GET)
+	public ResponseEntity<JSONResult> get_orders_by_admin(
+			@PathVariable(value="customer_no") Optional<Long> customer_no
+			, @PathVariable(value="product_detail_no") Optional<Long> product_detail_no) {
+		Long checked_customer_no = customer_no.isPresent()? customer_no.get(): null;
+		Long checked_product_detail_no = product_detail_no.isPresent()? product_detail_no.get(): null;
+
+		List<OrdersVo> orders_list = ordersService.get_orders_by_admin(checked_customer_no, checked_product_detail_no);
+		return ResponseEntity.status(HttpStatus.OK).body(JSONResult.success(orders_list));
+	}
+	
+	
+	// 일정 기간이 지나면 주문이 취소되는 것  QUARDZ로 구현 예정  
+	// 또는 주문 취소 버튼 클릭 
+	@ApiOperation(value = "주문 삭제")
+    @ApiImplicitParams({
+    	 @ApiImplicitParam(name = "orders_no", value = "orders_no", dataType = "long", paramType = "path"),
+    })
+	@ResponseBody
+	@RequestMapping(value = { "/{orders_no}" }, method = RequestMethod.DELETE)
+	public ResponseEntity<JSONResult> delete_orders(
+			@PathVariable(value="orders_no") Long orders_no) {
+		Boolean deleted_orders_no = ordersService.delete_orders(orders_no);
+		return ResponseEntity.status(HttpStatus.OK).body(JSONResult.success(deleted_orders_no));
+	}
+}
---
+++ shop_mall_API/src/main/java/com/cafe24/shop/vo/OrdersDetailVo.java
@@ -0,0 +1,124 @@
+package com.cafe24.shop.vo;
+
+public class OrdersDetailVo {
+
+	private Long no;
+	private Long order_no;
+	private Long product_detail_no;
+	private String prodcut_option;
+	private Long product_image_no;
+	private String product_image_url;
+	private Long category_no;
+	private Long brand_no;
+	private Long order_product_price;
+	private Long order_product_cnt;
+
+	public OrdersDetailVo(Long no, Long order_no, Long product_detail_no, String prodcut_option, Long product_image_no,
+			String product_image_url, Long category_no, Long brand_no, Long order_product_price,
+			Long order_product_cnt) {
+		super();
+		this.no = no;
+		this.order_no = order_no;
+		this.product_detail_no = product_detail_no;
+		this.prodcut_option = prodcut_option;
+		this.product_image_no = product_image_no;
+		this.product_image_url = product_image_url;
+		this.category_no = category_no;
+		this.brand_no = brand_no;
+		this.order_product_price = order_product_price;
+		this.order_product_cnt = order_product_cnt;
+	}
+
+	public OrdersDetailVo() {
+		super();
+	}
+
+	public Long getNo() {
+		return no;
+	}
+
+	public void setNo(Long no) {
+		this.no = no;
+	}
+
+	public Long getOrder_no() {
+		return order_no;
+	}
+
+	public void setOrder_no(Long order_no) {
+		this.order_no = order_no;
+	}
+
+	public Long getProduct_detail_no() {
+		return product_detail_no;
+	}
+
+	public void setProduct_detail_no(Long product_detail_no) {
+		this.product_detail_no = product_detail_no;
+	}
+
+	public String getProdcut_option() {
+		return prodcut_option;
+	}
+
+	public void setProdcut_option(String prodcut_option) {
+		this.prodcut_option = prodcut_option;
+	}
+
+	public Long getProduct_image_no() {
+		return product_image_no;
+	}
+
+	public void setProduct_image_no(Long product_image_no) {
+		this.product_image_no = product_image_no;
+	}
+
+	public String getProduct_image_url() {
+		return product_image_url;
+	}
+
+	public void setProduct_image_url(String product_image_url) {
+		this.product_image_url = product_image_url;
+	}
+
+	public Long getCategory_no() {
+		return category_no;
+	}
+
+	public void setCategory_no(Long category_no) {
+		this.category_no = category_no;
+	}
+
+	public Long getBrand_no() {
+		return brand_no;
+	}
+
+	public void setBrand_no(Long brand_no) {
+		this.brand_no = brand_no;
+	}
+
+	public Long getOrder_product_price() {
+		return order_product_price;
+	}
+
+	public void setOrder_product_price(Long order_product_price) {
+		this.order_product_price = order_product_price;
+	}
+
+	public Long getOrder_product_cnt() {
+		return order_product_cnt;
+	}
+
+	public void setOrder_product_cnt(Long order_product_cnt) {
+		this.order_product_cnt = order_product_cnt;
+	}
+
+	@Override
+	public String toString() {
+		return "OrdersDetailVo [no=" + no + ", order_no=" + order_no + ", product_detail_no=" + product_detail_no
+				+ ", prodcut_option=" + prodcut_option + ", product_image_no=" + product_image_no
+				+ ", product_image_url=" + product_image_url + ", category_no=" + category_no + ", brand_no=" + brand_no
+				+ ", order_product_price=" + order_product_price + ", order_product_cnt=" + order_product_cnt + "]";
+	}
+
+}

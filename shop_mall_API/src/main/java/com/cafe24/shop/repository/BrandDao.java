f41177786bddd9d9848d8ba6107ddadecde17cbb
MaximSungmo <ksm5318@naver.com>
2019-08-02 18:11:16 +0900

    BRAND TEST

---
+++ shop_mall_API/src/main/java/com/cafe24/shop/repository/BrandDao.java
@@ -0,0 +1,33 @@
+package com.cafe24.shop.repository;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.cafe24.shop.vo.BrandVo;
+
+@Repository
+public class BrandDao {
+	
+	@Autowired
+	private SqlSession sqlSession;
+
+	public Integer insert_brand(BrandVo brandvo) {
+		return sqlSession.insert("brand.insert_brand", brandvo);
+	}
+
+	public List<BrandVo> get_brand_list() {
+		return sqlSession.selectList("brand.get_brand_list");
+	}
+
+	public Integer update_brand(BrandVo brandvo) {
+		return sqlSession.update("brand.update_brand", brandvo);
+	}
+
+	public Integer delete_brand(BrandVo brandvo) {
+		return sqlSession.update("brand.delete_brand", brandvo);
+	}
+	
+}
---
+++ shop_mall_API/src/test/java/com/cafe24/shop/api/BrandControllerTest.java
@@ -0,0 +1,5 @@
+package com.cafe24.shop.api;
+
+public class BrandControllerTest {
+
+}

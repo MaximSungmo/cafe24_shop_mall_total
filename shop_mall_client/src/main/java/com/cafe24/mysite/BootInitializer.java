0420474c1ac9ccc181ef36feb505c841fcb38dea
MaximSungmo <ksm5318@naver.com>
2019-08-07 21:24:48 +0900

    실행파일 pom 수정, 버전 변경

--- shop_mall_client/src/main/java/com/cafe24/mysite/BootInitializer.java
+++ shop_mall_client/src/main/java/com/cafe24/mysite/BootInitializer.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+
 @EnableAutoConfiguration
 public class BootInitializer extends SpringBootServletInitializer {
 	@Override
